65b23e1783d4bc88e619144c78a0149c4730655a
sjangklint <sjangklint@gmail.com>
2022-04-24 18:00:29 +0200

    Added components, styling

---
+++ webapp/src/comps/Layout/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <a class="active" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+          </ul>
+        </nav>
+
+        </div>
+    );
+  }
+}
+
+export default Navbar;
