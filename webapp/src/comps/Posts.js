458e2f3a4c80004b7484a72fb1090882f9de069e
jennifersvensson1 <jennifer.svenssn@gmail.com>
2022-05-12 22:13:14 +0200

    Implements map from Google Maps with markers

--- webapp/src/comps/Posts.js
+++ webapp/src/comps/Posts.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { projectFirestore } from "../firebase/firebaseConfig";
 import DeletePost from "./DeletePost";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState({lat: 0, lng: 0})
 
   useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setCurrentLocation({ lat: pos.coords.latitude, lng: pos.coords.longitude});
+      })
+    }
+
     const postRef = collection(projectFirestore, "Posts");
     const q = query(postRef, orderBy("createdAt", "desc"));
     onSnapshot(q, (snapshot) => {
@@ -16,22 +24,48 @@ const Posts = () => {
       }));
       setPosts(posts);
       console.log(posts);
+
     });
   }, []);
+
+  const MyMapComponent = () => {
+    return (
+      <LoadScript 
+      // googleMapsApiKey="AIzaSyCIV1WJa_KN22TB4L3M0YNCbIdmAjxJk1E"
+      >
+        <GoogleMap
+          mapContainerStyle={{width: '900px', height: '600px'}}
+          center={currentLocation}
+          zoom={3}
+        >
+          {posts.map(({location}) => (
+            <Marker position={{lat: location.latitude, lng: location.longitude}} />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
+
   return (
-    <div className="card-container">
-      {posts.length === 0 ? (
-        <p>No posts found!</p>
-      ) : (
-        posts.map(({ id, title, imageUrl, createdAt }) => (
-          <div className="card" key={id}>
-            <img src={imageUrl} alt="title" />
-            <h2>{title}</h2>
-            <p>{createdAt.toDate().toDateString()}</p>
-            <DeletePost id={id} imageUrl={imageUrl} />
-          </div>
-        ))
-      )}
+    <div>
+      <div className="card-container">
+        {posts.length === 0 ? (
+          <p>No posts found!</p>
+        ) : (
+          posts.map(({ id, title, imageUrl, createdAt, location }) => (
+            <div className="card" key={id}>
+              <img src={imageUrl} alt="title" />
+              <h2>{title}</h2>
+              <p>{createdAt.toDate().toDateString()}</p>
+              <p>{location?.latitude}, {location?.longitude}</p>
+              <DeletePost id={id} imageUrl={imageUrl} />
+            </div>
+          ))
+        )}
+      </div>
+      <div className="map-container">
+        <MyMapComponent isMarkerShown />
+      </div> 
     </div>
   );
 };
