3b1cd271f26859335ec1335d25db6632a2fcdf37
jennifersvensson1 <jennifer.svenssn@gmail.com>
2022-05-15 22:30:54 +0200

    Configures firebase

--- webapp/src/firebase/firebaseConfig.js
+++ webapp/src/firebase/firebaseConfig.js
@@ -4,18 +4,17 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBQG7zJgouKBozQUg0BnD6v8gxPLrpIqtw",
-    authDomain: "reactgallery-ecd5d.firebaseapp.com",
-    projectId: "reactgallery-ecd5d",
-    storageBucket: "reactgallery-ecd5d.appspot.com",
-    messagingSenderId: "660847514958",
-    appId: "1:660847514958:web:ff2b9a099b72959f03ba1f"
+    apiKey: "AIzaSyABVVSPQ_v-TKm2omRnmUfwLJXebRrzk0E",
+    authDomain: "me305-assign3.firebaseapp.com",
+    projectId: "me305-assign3",
+    storageBucket: "me305-assign3.appspot.com",
+    messagingSenderId: "535373787715",
+    appId: "1:535373787715:web:ac1945fd8269c64a24daf6"
   };
   
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const projectStorage = getStorage(app);
 const projectFirestore = getFirestore(app);
-
 
 export { projectStorage, projectFirestore};
