cc68e87b2f69f8fe4d7409d658f5dc4d82bda681
jennifersvensson1 <jennifer.svenssn@gmail.com>
2022-05-15 21:36:41 +0200

    Implements deleting a post from mobile

--- app/components/portfolio.js
+++ app/components/portfolio.js
@@ -1,13 +1,15 @@
-
 import React, { useState, useEffect } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { projectFirestore } from '../firebase/firebaseConfig';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Button, Image, Modal, StyleSheet, Text, View } from 'react-native';
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { projectFirestore, projectStorage } from '../firebase/firebaseConfig';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { deleteObject, ref } from 'firebase/storage';
 
 // Initiates portfolio
 export const Portfolio = () => {
     const [posts, setPosts] =  useState([]);
+    const [modalText, setModalText] = useState("");
+	const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         const postRef = collection(projectFirestore, "Posts");
@@ -25,8 +27,29 @@ export const Portfolio = () => {
         }
     }, []);
 
+    async function deletePostAsync(id, imageUrl) {
+        try {
+            await deleteDoc(doc(projectFirestore, "Posts", id));
+            const storageRef = ref(projectStorage, imageUrl);
+            await deleteObject(storageRef);
+
+            setModalText('Post deleted successfully');
+            setModalVisible(true);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
       <View style={styles.container}>
+            <Modal animationType="slide" transparent={true} visible={modalVisible} style={{margin: 0}}>
+				<View style={styles.modalView}>
+				    <Text>{modalText}</Text>
+				    <Button title="OK" onPress={() => setModalVisible(false)}></Button>
+				</View>
+			</Modal>
+
             <ScrollView>
                 {posts.length === 0 ? 
                     ( <Text>No posts found!</Text>) : 
@@ -35,6 +58,9 @@ export const Portfolio = () => {
                             <Image style={styles.cardImg} source={{ uri: imageUrl, }} />
                             <Text style={styles.cardTitle}>{title}</Text>
                             <Text>{createdAt.toDate().toDateString()}</Text>
+                            <TouchableOpacity style={styles.deleteBtn} onPress={() => { deletePostAsync(id, imageUrl) }}>
+                                <Text style={styles.btnText}>Delete</Text>
+                            </TouchableOpacity>
                         </View>
                     )))
                 }
@@ -65,5 +91,28 @@ const styles = StyleSheet.create({
     cardTitle: {
         fontSize: 20,
         padding: 10
-    }
+    },
+    deleteBtn: {
+		padding: 5,
+		width: 80,
+		margin: 10,
+        alignItems: 'center'
+	},
+	btnText: {
+		fontSize: 16,
+        color: '#dc3545'
+	},
+    modalView: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'whitesmoke',
+		shadowColor: '#000',
+		shadowOpacity: 0.25,
+		shadowOffset: {
+		  width: 0,
+		  height: 2
+		},
+		borderRadius: 10
+	}
   });
